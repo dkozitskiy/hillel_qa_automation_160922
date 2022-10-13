9d252ee378ae6f2c8a82ce9c70b592b64156c1da
Denis_Kozitskiy <dkozitskiy@gmail.com>
2022-10-13 22:53:21 +0300

    added answers to homework 8

---
+++ lesson8.py
@@ -0,0 +1,21 @@
+def change_to_string(func):
+    """
+    This decorator that receives a function executes it. After that returns the result of the function as a string
+    :param func: takes a function
+    :return: returns the result of the received function as a string
+    """
+
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        for arg in args:
+            if not isinstance(arg, str):
+                return str(result)
+            else:
+                return result
+        for k, v in kwargs:
+            if not isinstance(v, str):
+                return str(result)
+            else:
+                return result
+
+    return wrapper
