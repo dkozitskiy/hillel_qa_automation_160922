3ccdb34da7d4a17cda91d4b8ffd5c1f21f0c73fe
Denis_Kozitskiy <dkozitskiy@gmail.com>
2022-11-18 22:56:16 +0200

    automation. added solution 1

---
+++ automation_lessons/less1/locators1.py
@@ -0,0 +1,52 @@
+"""
+https://uk.wikipedia.org/wiki/Python
+
+XPath:
+1.  //a[@class="mw-wiki-logo"]
+2.  //li[@id="n-mainpage-description"]//span
+3.  //li[@id="pt-login"]/a
+4.  //span[@class="mw-page-title-main"]
+5.  //li[@id="pt-anoncontribs"]/a
+6.  //ul[@class="vector-menu-content-list"]//li[@id="pt-anoncontribs"]//a
+7.  //div[@class="vector-menu-content"]//li[@id="pt-anoncontribs"]//a
+8.  //li[@id="pt-anoncontribs"]/a/span
+9.  //div[@class="toctitle"]/span
+10.  //li[@id="pt-login"]/a/span
+11.  //li[@id="n-mainpage-description"]/a
+12.  //li[contains(@id,'specialpages')]/a
+13.  //button[contains(@class,'settings')]
+14.  //li[@id="pt-createaccount"]/a
+15.  //input[contains(@class,"vector-search-box-input")]
+16.  //div[@class="thumbinner"]//img[contains(@alt,"The standard type hierarchy.png")]
+17.  //img[@alt="Python logo and wordmark.svg"]
+18.  //a[@href="/w/index.php?title=Python&veaction=edit"]
+19.  //li[@id="ca-ve-edit"]/a
+20.  //div[contains(@id,'logo')]/a[contains(@class,'wiki-logo')]
+21.  //a[@rel="discussion"]
+22.  //div[@id="simpleSearch"]/input[@id="searchButton"]
+23.  //div[@id="left-navigation"]//a[@href="/wiki/Python"]
+24.  //li[@id="footer-places-about"]/a
+25.  //li[@id="footer-info-copyright"]//a[2]
+26.  //span[@portal-box]/b/a
+27.  //span[@portal-box]//child::a
+28.  //span/a[@class="extiw"]
+29.  //span[@class="wikibooks-ref"]/a
+30.  //input[@type="search" or name="search"]
+
+CSS:
+1.  input[name="search"]
+2.  #p-logo>a
+3.  #ca-talk>a
+4.  #ca-history >a
+5.  .vector-menu-content-list>#n-mainpage-description>a
+6.  div[class="vector-menu-content"] #n-currentevents>a
+7.  ul li[class="toclevel-1 tocsection-1"]>a
+8.  #p-lang> .uls-settings-trigger
+9.  .portal-box a
+10.  span a[href="https://uk.wikibooks.org/wiki/Python"]
+11.  tr span[class="wikicommons-ref"]>a
+12.  #footer-places-privacy>a
+13.  .collapseButton>#collapseButton0
+14.  #footer-copyrightico a[href]
+15.  .vector-menu-content-list>button
+"""
