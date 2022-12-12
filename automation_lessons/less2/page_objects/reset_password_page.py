b1d806dd2ac01163d96143af315ee441f723f9c0
DenisDK <dkozitskiy@gmail.com>
2022-12-12 03:24:46 +0200

    add test_left_menu_in_login_page

--- automation_lessons/less2/page_objects/reset_password_page.py
+++ automation_lessons/less2/page_objects/reset_password_page.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.common.by import By
+
 from automation_lessons.less2.utilities.web_ui.base_page import BasePage
 
 
@@ -7,6 +9,10 @@ class ResetPasswordPage(BasePage):
         super().__init__(self.driver)
 
     __title = 'Скинути пароль — Вікіпедія'
+    __left_menu = (By.XPATH, '//div[@id="mw-panel"]')
 
     def title(self):
         return self.check_title(self.__title)
+
+    def left_menu_is_visible(self):
+        return self._is_visible(self.__left_menu)
--- automation_lessons/less2/tests/test_reset_password_page.py
+++ automation_lessons/less2/tests/test_reset_password_page.py
@@ -4,3 +4,8 @@ import pytest
 @pytest.mark.regression
 def test_title_name(open_reset_password_page):
     assert open_reset_password_page.title() is True
+
+
+@pytest.mark.regression
+def test_left_menu_in_login_page(open_reset_password_page):
+    assert open_reset_password_page.left_menu_is_visible() is True
