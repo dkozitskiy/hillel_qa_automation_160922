9a2acdf21871043f3643607232f0c3cb6f0ff53b
DenisDK <dkozitskiy@gmail.com>
2022-12-19 01:52:42 +0200

    dell old fixture in conftest

--- automation_lessons/my_framework/conftest.py
+++ automation_lessons/my_framework/conftest.py
@@ -8,20 +8,6 @@ from automation_lessons.my_framework.utilities.config_parser import ReadConfig
 from automation_lessons.my_framework.utilities.driver_factory import DriverFactory
 
 
-# @pytest.fixture()
-# def create_driver():
-#     driver = DriverFactory.create_driver(ReadConfig.get_browser_id())
-#     driver.maximize_window()
-#     driver.get(ReadConfig.get_base_url())
-#     yield driver
-#     driver.quit()
-#
-#
-# @pytest.fixture()
-# def open_login_page(create_driver):
-#     return LoginPage(create_driver)
-
-
 @pytest.fixture()
 def create_driver():
     driver = DriverFactory.create_driver(ReadConfig.get_browser_id())
