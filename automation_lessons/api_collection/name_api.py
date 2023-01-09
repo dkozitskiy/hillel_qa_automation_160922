471047da6e11166b7172c435911c2bcbc15b109b
DenisDK <dkozitskiy@gmail.com>
2023-01-09 02:45:09 +0200

    updated "gender" according to the example shown in the lesson

--- automation_lessons/api_collection/name_api.py
+++ automation_lessons/api_collection/name_api.py
@@ -1,3 +1,4 @@
+from automation_lessons.data.gender import Gender
 from automation_lessons.my_framework.utilities.api.BaseAPI import BaseAPI
 
 
@@ -9,4 +10,11 @@ class NameAPI(BaseAPI):
     def get_gender_by_name(self, base_url, name):
         return self.get(base_url, f'{self.__url}{name}')
 
-
+    def create_gender(self, base_url, body=None):
+        gender_data = Gender()
+        if body is not None:
+            gender_data = gender_data
+        else:
+            gender_data.update_dict(body)
+        response = self.post(base_url, body=gender_data.get_json())
+        return response
--- automation_lessons/data/gender.py
+++ automation_lessons/data/gender.py
@@ -1,13 +1,25 @@
+import json
+
+
 class Gender:
-    def __init__(self, count, gender, name, probability):
-        self.count = count
-        self.gender = gender
-        self.name = name
-        self.probability = probability
+    def __init__(self, **kwargs):
+        self.count = 168019 if 'count' not in kwargs.keys() else kwargs['count']
+        self.gender = 'male' if 'gender' not in kwargs.keys() else kwargs['gender']
+        self.name = 'Igor' if 'name' not in kwargs.keys() else kwargs['name']
+        self.probability = 1.0 if 'probability' not in kwargs.keys() else kwargs['probability']
 
     @classmethod
     def create_from_json(cls, **kwargs):
-        return cls(kwargs['count'], kwargs['gender'], kwargs['name'], kwargs['probability'])
+        return cls(**kwargs)
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
+
+    def update_dict(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+    def get_json(self):
+        return json.dumps(self.__dict__)
+
+    def get_dikt(self):
+        return json.loads(self.__dict__)
